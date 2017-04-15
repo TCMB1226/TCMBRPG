fd31a86882ad9f89017064c65bc83d18479fb784
Corey Chilton <simfrek@gmail.com>
2017-04-15 11:01:29 +0100

    Start Project
    added folders, fixed layout, added scene with basic gui

---
+++ New Unity Project/Assets/Scripts/Control.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+
+public class Control : MonoBehaviour {
+
+    [System.Serializable]
+    public class Options
+    {
+        public string text;
+        public GameObject Destination;
+    }
+
+    public string Story;
+    public List<Options> options = new List<Options>();
+
+    // Use this for initialization
+    void Start ()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ New Unity Project/Assets/Scripts/Master.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Master : MonoBehaviour {
+
+    float optionSpacing = 10;
+    public GameObject CurrentStage;
+    public GameObject Story;
+    public GameObject Option0;
+    private List<GameObject> options = new List<GameObject>();
+    public bool Updated;
+    
+
+	// Use this for initialization
+	void Start () {
+        var Stage = CurrentStage.GetComponent<Control>();
+        Story.GetComponent<Text>().text = Stage.Story;
+        optionSpacing += Option0.GetComponent<RectTransform>().rect.height;
+        for (int i = 0; i < Stage.options.Capacity ; i++)
+        {
+            GameObject clone;
+            clone = Instantiate(Option0, Option0.transform.parent) as GameObject;
+            clone.transform.position = Option0.transform.position;
+            clone.name = "Option" + i;
+            var clonepos = clone.GetComponent<RectTransform>().anchoredPosition;
+            clone.GetComponent<RectTransform>().anchoredPosition = new Vector2(clonepos.x, clonepos.y - optionSpacing * i);
+            clone.GetComponent<Text>().text = Stage.options[i].text;
+            options.Add(clone);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Updated)
+            changed();
+	}
+
+    public void changed()
+    {
+        Updated = false;
+        while (options.Count > 0)
+        {
+            var obj = options[0];
+            options.Remove(obj);
+            
+        }
+
+        var Stage = CurrentStage.GetComponent<Control>();
+        Story.GetComponent<Text>().text = Stage.Story;
+        for (int i = 0; i < Stage.options.Capacity; i++)
+        {
+            GameObject clone;
+            clone = Instantiate(Option0) as GameObject;
+            clone.transform.SetParent(Option0.transform.parent);
+            clone.transform.position = Option0.transform.position;
+            clone.name = "Option" + i;
+            var clonepos = clone.GetComponent<RectTransform>().anchoredPosition;
+            clone.GetComponent<RectTransform>().anchoredPosition = new Vector2(clonepos.x, clonepos.y - optionSpacing * i);
+            clone.GetComponent<Text>().text = Stage.options[i].text;
+        }
+    }
+
+}
