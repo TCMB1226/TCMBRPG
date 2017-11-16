f04fbdfb2a284c0d02a0d44c3c63bcf08a900960
Corey Chilton <simfrek@gmail.com>
2017-11-16 00:03:55 +0000

    Basic Scenes

---
+++ New Unity Project/Assets/Scripts/Combat.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Combat : MonoBehaviour {
+    public GameObject Attacker;
+    public GameObject Fighter;
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
