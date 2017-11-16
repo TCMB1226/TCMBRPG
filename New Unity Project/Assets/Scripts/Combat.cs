0e3586a95893c456045169e448786f8c4adcd49c
Corey Chilton <simfrek@gmail.com>
2017-11-16 00:46:47 +0000

    Stats Scripts linked to combat scripts

--- New Unity Project/Assets/Scripts/Combat.cs
+++ New Unity Project/Assets/Scripts/Combat.cs
@@ -4,12 +4,19 @@ using System.Collections;
 public class Combat : MonoBehaviour {
     public GameObject Attacker;
     public GameObject Defender;
+    public int minAttackHP = 2;
+    public int minAttackStamina = 1;
+    Stats defender;
+    Stats attacker;
 
 
     // Use this for initialization
-    void Start () {
-	    
-	}
+    void Start ()
+    {
+        defender = Defender.GetComponent<Stats>();
+        attacker = Attacker.GetComponent<Stats>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -18,6 +25,29 @@ public class Combat : MonoBehaviour {
 
     void Attack()
     {
-        
+        if (attacker.Stamina >= minAttackStamina)
+        {
+            if (attacker.Health >= minAttackHP)
+            {
+
+                if (defender.Defence < attacker.Damage)
+                {
+                    attacker.Health -= 2;
+                    defender.Health -= 10;
+                    return;
+                }
+                else
+                {
+                    attacker.Health -= 15;
+                    defender.Health -= 1;
+                    return;
+                }
+            }
+            else
+                return;
+        }
+        else
+            return;
+
     }
 }
